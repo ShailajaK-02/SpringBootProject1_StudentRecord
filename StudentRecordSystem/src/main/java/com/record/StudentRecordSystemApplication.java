ae7c522ed67accfbc99d19e83e2e3948ca2b8384
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-03 00:43:47 +0530

    main class implemented

--- StudentRecordSystem/src/main/java/com/record/StudentRecordSystemApplication.java
+++ StudentRecordSystem/src/main/java/com/record/StudentRecordSystemApplication.java
@@ -3,6 +3,7 @@ package com.record;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main class
 @SpringBootApplication
 public class StudentRecordSystemApplication {
 
